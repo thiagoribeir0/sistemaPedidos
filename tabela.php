a33bb280394f6ebb00850dc645adc9acbd5796ee
Thiago <thiagobarbosaribeiro@edu.uniube.br>
2024-05-24 13:52:04 -0300

    update e delete funcionando \o/

--- tabela.php
+++ tabela.php
@@ -2,12 +2,32 @@
 include('conexao.php');
 include('protect.php');
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST['action']) && isset($_POST['produto_id'])) {
+        $action = $_POST['action'];
+        $produto_id = $_POST['produto_id'];
+
+        if($action == "update") {
+            $nomeProduto = $_POST["nomeProduto"];
+            $quantidade = $_POST["quantidade"];
+            $valor = $_POST["valor"];
+
+        $sql_update = "UPDATE produtos SET nomeProduto='$nomeProduto', quantidade='$quantidade', valor='$valor' WHERE id='$produto_id'";
+        $mysqli->query($sql_update) or die ("Falha na execução do código SQL: " . $mysqli->error);
+    
+    } elseif($action == "delete") {
+            $sql_delete = "DELETE FROM produtos WHERE id='$produto_id'";
+            $mysqli->query($sql_delete) or die("Falha na execução do código SQL: " . $mysqli->error);
+        }
+    }
+}
+
 $selectProdutos = "SELECT * FROM produtos";
 $sql_query = $mysqli->query($selectProdutos) or die("Falha na execução do código SQL: " . $mysqli->error);
 
 function exibirProdutos() {
     global $sql_query;
-    if ($sql_query->num_rows > 0){
+    if ($sql_query->num_rows > 0) {
         echo "
                 <table>
                 <tr>
@@ -18,11 +38,16 @@ function exibirProdutos() {
             ";
         while ($row = $sql_query->fetch_assoc()) {
             echo "<tr>";
-            echo "<td>" . "<input type='text' class='$row[id]' value='$row[nomeProduto]'>" . "</td>";
-            echo "<td>" . "<input type='text' class='$row[id]' value='$row[quantidade]'>" . "</td>";
-            echo "<td>" . "<input type='text' class='$row[id]' value='$row[valor]'>" . "</td>";
-            echo "<td>" . "<input type='submit' value='Salvar alterações'>" . "</td>";
-            echo "<td>" . "<input type='submit' value='Apagar'>" . "</td>";
+            echo "<form method='post'>";
+            echo "<td><input type='text' name='nomeProduto' value='" . $row['nomeProduto'] . "'></td>";
+            echo "<td><input type='text' name='quantidade' value='" . $row['quantidade'] . "'></td>";
+            echo "<td><input type='text' name='valor' value='" . $row['valor'] . "'></td>";
+            echo "<td>
+                <input type='hidden' name='produto_id' value='" . $row['id'] . "'>
+                <button type='submit' name='action' value='update'>Salvar alterações</button>
+                <button type='submit' name='action' value='delete'>Deletar</button>
+                </td>";
+            echo "</form>";
             echo "</tr>";
         }
         echo "</table>";
